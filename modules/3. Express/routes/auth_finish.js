a2a877d9c70c4d73b5f5f20db9765422a18117e9
aetheryx <zain1@live.nl>
2018-01-07 18:50:03 +0100

    add placeholder

--- modules/3. Express/routes/auth_finish.js
+++ modules/3. Express/routes/auth_finish.js
@@ -1,6 +1,6 @@
 async function init () {
   this.app.get('/auth/finish', async (req, res) => {
-    res.send(`Your Discord account (tag ${(await this.bot.fetchUser(req.session.discordID)).tag}) has been successfully bound with your Twitter account (@${req.session.OAuthAccessResults.screen_name}).
+    res.send(`Your Discord account (ID ${req.session.discordID}, tag sporksmumgay (placeholder)) has been successfully bound with your Twitter account (@${req.session.OAuthAccessResults.screen_name}).
   You can use the bot now.`);
   });
 }

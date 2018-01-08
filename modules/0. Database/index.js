858969aa97c1b66841af504e89c0f80df6c6f41b
aetheryx <zain1@live.nl>
2018-01-09 00:13:52 +0100

    add modules in proper order

--- modules/0. Database/index.js
+++ modules/0. Database/index.js
@@ -1,26 +1,25 @@
 const { MongoClient } = require('mongodb');
 const dbFunctions = require(`${__dirname}/dbFunctions`);
 
-function init () {
-  MongoClient.connect(this.config.dbURL || 'mongodb://localhost:27017')
-    .then(dbClient => {
-      this.dbClient = dbClient;
-      this.dbConn = dbClient.db('tweetcord');
+async function init () {
+  this.dbClient = await MongoClient.connect(this.config.dbURL || 'mongodb://localhost:27017')
+    .catch(e => {
+      this.log(`Failed to connect to MongoDB: ${e.message}\nExiting...`, 'error');
+      process.exit();
+    });
 
-      this.dbTables = {
-        'prefixes': this.dbConn.collection('prefixes'),
-        'links': this.dbConn.collection('links')
-      };
+  this.dbConn = this.dbClient.db('tweetcord');
 
-      this.db = {};
-      for (const dbFunction in dbFunctions) {
-        this.db[dbFunction] = dbFunctions[dbFunction].bind(this);
-      }
-    })
-    // .catch(e => {
-    //   this.log(`Failed to connect to MongoDB: ${e.message}\nExiting...`, 'error');
-    //   process.exit();
-    // });
+  this.dbTables = {
+    'prefixes': this.dbConn.collection('prefixes'),
+    'links': this.dbConn.collection('links'),
+    'timelines': this.dbConn.collection('timelines')
+  };
+
+  this.db = {};
+  for (const dbFunction in dbFunctions) {
+    this.db[dbFunction] = dbFunctions[dbFunction].bind(this);
+  }
 }
 
 module.exports = init;
--- modules/1. Bot/index.js
+++ modules/1. Bot/index.js
@@ -1,81 +1,87 @@
 const { Client } = require('eris');
 const fs = require('fs');
 const events = require(`${__dirname}/events`);
+const MessageCollector = require(`${__dirname}/misc/MessageCollector.js`);
 
 function init () {
-  const _this = this;
+  return new Promise(resolve => {
+    const _this = this;
 
-  class Bot extends Client {
-    constructor (props) {
-      super(_this.config.bot.token, props);
-      this.connect();
+    class Bot extends Client {
+      constructor (props) {
+        super(_this.config.bot.token, props);
+        this.connect();
 
-      this.commands = {};
-      this.loadCommands();
+        this.commands = {};
+        this.loadCommands();
 
-      this
-        .on('ready', events.onReady.bind(_this))
-        .on('messageCreate', events.onMessageCreate.bind(_this));
-    }
+        this.MessageCollector = new MessageCollector(this);
 
-    async loadCommands () {
-      fs.readdir(`${__dirname}/commands`, (err, files) => {
-        if (err) {
-          return this.log(err.stack, 'error');
-        }
+        this
+          .once('ready', resolve)
+          .on('ready', events.onReady.bind(_this))
+          .on('messageCreate', events.onMessageCreate.bind(_this));
+      }
 
-        let failed = 0;
-
-        files.forEach(file => {
-          try {
-            const command = require(`${__dirname}/commands/${file}`);
-            this.commands[command.name] = Object.assign({
-              aliases: [],
-              ownerOnly: false,
-              usage: '{command}'
-            }, command);
-          } catch (err) {
-            failed++;
-            _this.log(`Failed to load command ${file}: \n${err.stack}`, 'error');
+      async loadCommands () {
+        fs.readdir(`${__dirname}/commands`, (err, files) => {
+          if (err) {
+            return this.log(err.stack, 'error');
           }
+
+          let failed = 0;
+
+          files.forEach(file => {
+            try {
+              const command = require(`${__dirname}/commands/${file}`);
+              this.commands[command.name] = Object.assign({
+                aliases: [],
+                ownerOnly: false,
+                usage: '{command}'
+              }, command);
+            } catch (err) {
+              failed++;
+              _this.log(`Failed to load command ${file}: \n${err.stack}`, 'error');
+            }
+          });
+
+          _this.log(`Successfully loaded ${files.length - failed}/${files.length} commands.`);
         });
 
-        _this.log(`Successfully loaded ${files.length - failed}/${files.length} commands.`);
-      });
-
-    }
-
-    async sendMessage (target, content, isUser = false) {
-      if (content instanceof Object && !content.content) {
-        content = { embed: content };
       }
 
-      if (content.embed && !content.embed.color) {
-        content.embed.color = _this.config.bot.embedColor;
-      }
-
-      try {
-        if (isUser) {
-          const DMChannel = await this.getDMChannel(target);
-          return await DMChannel.createMessage(content); // return await is okay here
-        } else {                                        //  because we're in a try-catch
-          return await this.createMessage(target, content);
+      async sendMessage (target, content, isUser = false) {
+        if (content instanceof Object && !content.content) {
+          content = { embed: content };
         }
-      } catch (err) {
-        if (
-          !err.message.includes('Missing Permissions') && // TODO: re-test these and replace these strings with HTTP codes
-          !err.message.includes('Cannot send messages to this user') &&
-          !err.message.includes('Missing Access')
-        ) {
-          _this.log(`Unrecognized error: ${err.stack}\n${content}`, 'error');
-        } else {
-          return false;
+
+        if (content.embed && !content.embed.color) {
+          content.embed.color = _this.config.bot.embedColor;
+        }
+
+        try {
+          if (isUser) {
+            const DMChannel = await this.getDMChannel(target);
+            return await DMChannel.createMessage(content); // return await is okay here
+          } else {                                        //  because we're in a try-catch
+            return await this.createMessage(target, content);
+          }
+        } catch (err) {
+          if (
+            !err.message.includes('Missing Permissions') && // TODO: re-test these and replace these strings with HTTP codes
+            !err.message.includes('Cannot send messages to this user') &&
+            !err.message.includes('Missing Access')
+          ) {
+            _this.log(`Unrecognized error: ${err.stack}\n${content}`, 'error');
+          } else {
+            return false;
+          }
         }
       }
     }
-  }
 
-  this.bot = new Bot();
+    this.bot = new Bot();
+  });
 }
 
 module.exports = init;

242ddcf6a0f13101133bbe704e945565acbd965d
aetheryx <zain1@live.nl>
2018-01-09 00:14:16 +0100

    add alias, timestamp and char limit

--- modules/1. Bot/commands/tweet.js
+++ modules/1. Bot/commands/tweet.js
@@ -7,6 +7,9 @@ async function tweetCommand (msg, args) {
     return `You haven't linked your Twitter account yet. Please do here: ${this.config.web.domain}/link?id=${msg.author.id}`;
   }
 
+  if (args.length > 280) {
+    return `Your tweet is too big! You're ${280 - args.length} characters over the limit.`;
+  }
 
   const res = await this.RestClient.tweet(
     linkInfo.OAuthAccessToken,
@@ -14,16 +17,15 @@ async function tweetCommand (msg, args) {
     args
   ).catch(e => {
     if (e.data.includes('"code":187')) {
-      this.bot.sendMessage(msg.channel.id, {
-        description: 'You\'ve already posted this tweet before.'
-      });
+      this.bot.sendMessage(msg.channel.id, 'You\'ve already posted this tweet before.');
     }
   });
 
   if (res) {
     return {
-      title: 'Tweet Sent!',
-      description: `[View here](https://twitter.com/${res.user.screen_name}/status/${res.id_str})`
+      title: 'Tweet successfuly sent',
+      description: `View [here](https://twitter.com/${res.user.screen_name}/status/${res.id_str})`,
+      timestamp: new Date()
     };
   }
 }
@@ -31,5 +33,6 @@ async function tweetCommand (msg, args) {
 module.exports = {
   command: tweetCommand,
   name: 'tweet',
+  aliases: ['weet'],
   description: ''
 }

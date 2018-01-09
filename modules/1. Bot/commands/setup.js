b39a1f89dab637b73c0cb4d57ef96127350030dd
aetheryx <zain1@live.nl>
2018-01-09 21:55:24 +0100

    add extra verification layer to setup

--- modules/1. Bot/commands/setup.js
+++ modules/1. Bot/commands/setup.js
@@ -3,9 +3,9 @@ async function setupCommand (msg) {
     return 'You need to mention a channel.';
   }
 
-  const linkInfo = await this.db.getLink(msg.author.id);
+  const link = await this.db.getLink(msg.author.id);
 
-  if (!linkInfo) {
+  if (!link) {
     return `You haven't linked your Twitter account yet. Please do here: ${this.config.web.domain}/link?id=${msg.author.id}`;
   }
 
@@ -14,12 +14,23 @@ async function setupCommand (msg) {
   if (potentialTimeline) {
     const channel = potentialTimeline.channelID === msg.channelMentions[0];
     return channel ?
-      `The channel <#${msg.channelMentions}> has already been linked with ${msg.author.id === potentialTimeline.userID && 'you' || await this.RestClient.getTagByID(potentialTimeline.userID)}.` :
+      `The channel <#${msg.channelMentions}> has already been linked with ${msg.author.id === potentialTimeline.userID ? 'you' : await this.RestClient.getTagByID(potentialTimeline.userID)}.` :
       `You've already linked with <#${potentialTimeline.channelID}>.`;
   }
 
-  await this.db.addTimeline(msg.channelMentions[0], msg.author.id);
-  return `<#${msg.channelMentions[0]}> has been successfully set up for your timeline. Any new tweets will now appear there.`;
+  this.bot.sendMessage(msg.channel.id, `Are you sure you want to link <#${msg.channelMentions[0]}> with your twitter account (\`@${link.name}\`)?\nThis means anyone who can see <#${msg.channelMentions[0]}> will be able to read any new tweets on your timeline.\n\nRespond with \`yes\` or \`no\`.`);
+
+  const message = await this.bot.MessageCollector.awaitMessage(msg.channel.id, msg.author.id, 5e3, (m) => ['y', 'n', 'yes', 'no'].includes(m.content.toLowerCase()));
+  if (!message) {
+    return 'Prompt timed out.';
+  }
+
+  if (['n', 'no'].includes(message.content.toLowerCase())) {
+    return 'Cancelled.';
+  } else {
+    await this.db.addTimeline(msg.channelMentions[0], msg.author.id);
+    return `<#${msg.channelMentions[0]}> has been successfully set up for your timeline. Any new tweets will now appear there.`;
+  }
 }
 
 module.exports = {

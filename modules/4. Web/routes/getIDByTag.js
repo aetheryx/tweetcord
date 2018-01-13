517d9f2f9245c9f03be5c675df6cedd2ebdc4cdf
aetheryx <zain1@live.nl>
2018-01-13 01:46:51 +0100

    fix links

--- modules/4. Web/routes/getIDByTag.js
+++ modules/4. Web/routes/getIDByTag.js
@@ -2,7 +2,7 @@ async function init () {
   this.app.get('/getIDByTag', async (req, res) => {
     const result = this.bot.users.find(u => `${u.username}#${u.discriminator}` === req.headers.tag);
     if (!result) {
-      res.status(404).send('404 Not Found');
+      res.status(404).send('404');
     } else {
       res.status(200).send(result.id);
     }

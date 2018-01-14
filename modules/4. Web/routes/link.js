6196cf386e603a5e98e7c5e0926da8b7e43f0315
aetheryx <zain1@live.nl>
2018-01-14 02:00:19 +0100

    a little bit of validation

--- modules/4. Web/routes/link.js
+++ modules/4. Web/routes/link.js
@@ -1,6 +1,14 @@
 async function init () {
   this.app.get('/link', async (req, res) => {
-    res.render('link', { id: req.query.id, tag: await this.RestClient.getTagByID(req.query.id) });
+    if (!req.query.id) {
+      return res.status(400).send('Missing ID querystring');
+    }
+
+    const tag = await this.RestClient.getTagByID(req.query.id);
+    if (!tag) {
+      return res.status(404).send('ID not found');
+    }
+    res.render('link', { id: req.query.id, tag });
   });
 }
 

f6640626edfa98fc9371291fec824b555d330637
aetheryx <zain1@live.nl>
2018-01-14 01:56:47 +0100

    this can be made prettier

--- modules/3. Bot/commands/exec.js
+++ modules/3. Bot/commands/exec.js
@@ -9,7 +9,7 @@ async function execCommand (msg, args) {
     if (!stderr && !stdout) {
       msg.react('\u2611');
     } else {
-      this.bot.sendMessage(msg.channel.id, `${stdout ? `Info: \`\`\`\n${stdout}\n\`\`\`` : ''}\n${stderr ? `Errors: \`\`\`\n${stderr}\`\`\`` : ''}`);
+      return `${stdout ? `Info: \`\`\`\n${stdout}\n\`\`\`` : ''}\n${stderr ? `Errors: \`\`\`\n${stderr}\`\`\`` : ''}`;
     }
   });
 }

ced4fd6c693e26ee9aa1c3522998b9b64c5d23bb
aetheryx <zain1@live.nl>
2018-02-05 16:19:01 +0100

    cleanup

--- modules/OAuthClient/index.js
+++ modules/OAuthClient/index.js
@@ -1,56 +1,57 @@
 const methods = require(`${__dirname}/methods`);
 
-class OAuthClient {
-  constructor (mainClass) {
-    this.mainClass = mainClass;
-    this.keys = mainClass.config.twitter;
-    this.percentEncode = mainClass.utils.percentEncode;
-
-    for (const method in methods) {
-      this[method] = methods[method].bind(mainClass);
-    }
-  }
-
-  signHeaders (method, url, props = {}, secret = '') {
-    let headers = [];
-    for (const prop in props) {
-      headers.push([ prop, props[prop] ]);
-    }
-    headers.push(
-      [ 'oauth_callback', `${this.mainClass.config.web.domain}/auth/callback` ],
-      [ 'oauth_consumer_key', this.keys.APIKey ],
-      [ 'oauth_nonce', this.mainClass.utils.randomString(32) ],
-      [ 'oauth_timestamp', (Date.now() / 1000).toFixed() ],
-      [ 'oauth_signature_method', 'HMAC-SHA1' ],
-      [ 'oauth_version', '1.0' ]
-    );
-    headers = headers
-      .map(header => header.map(this.percentEncode))
-      .sort((a, b) => {
-        if (a[0] < b[0]) {
-          return -1;
-        } else if (a[0] > b[0]) {
-          return 1;
-        } else {
-          return 0;
-        }
-      }).map(header => `${header[0]}="${header[1]}"`);
-
-    const paramString = this.percentEncode(headers.join('&').replace(/"/g, ''));
-
-    const signature = this.mainClass.utils.encrypt.hashHmac(
-      `${method.toUpperCase()}&${this.percentEncode(url.startsWith('https://') ? url : `https://${url}`)}&${paramString}`,
-      `${this.percentEncode(this.keys.secret)}&${this.percentEncode(secret)}`
-    );
-
-    headers.push(`oauth_signature="${this.percentEncode(signature)}"`);
-    return headers.filter(h => h.startsWith('oauth'));
-  }
-}
-
 module.exports = {
   order: 1,
   func: async function createOAuthClient () {
-    this.OAuthClient = new OAuthClient(this);
+    const _this = this;
+
+    class OAuthClient {
+      constructor () {
+        this.keys = _this.config.twitter;
+        this.percentEncode = _this.utils.percentEncode;
+
+        for (const method in methods) {
+          this[method] = methods[method].bind(_this);
+        }
+      }
+
+      signHeaders (method, url, props = {}, secret = '') {
+        let headers = [];
+        for (const prop in props) {
+          headers.push([ prop, props[prop] ]);
+        }
+        headers.push(
+          [ 'oauth_callback',         `${_this.config.web.domain}/auth/callback` ],
+          [ 'oauth_consumer_key',     this.keys.APIKey                           ],
+          [ 'oauth_nonce',            _this.utils.randomString(32)               ],
+          [ 'oauth_timestamp',        (Date.now() / 1000).toFixed()              ],
+          [ 'oauth_signature_method', 'HMAC-SHA1'                                ],
+          [ 'oauth_version',          '1.0'                                      ]
+        );
+        headers = headers
+          .map(header => header.map(this.percentEncode))
+          .sort((a, b) => {
+            if (a[0] < b[0]) {
+              return -1;
+            } else if (a[0] > b[0]) {
+              return 1;
+            } else {
+              return 0;
+            }
+          }).map(header => `${header[0]}="${header[1]}"`);
+
+        const paramString = this.percentEncode(headers.join('&').replace(/"/g, ''));
+
+        const signature = _this.utils.encrypt.hashHmac(
+          `${method.toUpperCase()}&${this.percentEncode(url.startsWith('https://') ? url : `https://${url}`)}&${paramString}`,
+          `${this.percentEncode(this.keys.secret)}&${this.percentEncode(secret)}`
+        );
+
+        headers.push(`oauth_signature="${this.percentEncode(signature)}"`);
+        return headers.filter(h => h.startsWith('oauth'));
+      }
+    }
+
+    this.OAuthClient = new OAuthClient();
   }
-}
+};

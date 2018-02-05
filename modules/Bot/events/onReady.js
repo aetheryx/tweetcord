c44efbce715c9317c06f8b167950fb5c7f8316e3
aetheryx <zain1@live.nl>
2018-02-05 22:03:12 +0100

    add help command to status

--- modules/Bot/events/onReady.js
+++ modules/Bot/events/onReady.js
@@ -1,5 +1,8 @@
 async function onReady () {
   this.log('Bot successfully logged in');
+  this.bot.editStatus('online', {
+    name: `${this.config.bot.defaultPrefix}help`
+  });
 }
 
 module.exports = onReady;

1d955eaadbf773cfa6876d8a6009b42be1299c87
aetheryx <zain1@live.nl>
2018-02-19 00:38:06 +0100

    hold data as buffers so we can manipulate it afterwards

--- modules/Utils/utils/get.js
+++ modules/Utils/utils/get.js
@@ -2,21 +2,22 @@ const { request } = require('https');
 
 function get (options) {
   return new Promise((resolve, reject) => {
-    const url = options.url.split('/');
+    const url = options.url.replace('https://', '').split('/');
     options = Object.assign({
       hostname: url.shift(),
       headers: Object.assign({ 'User-Agent': 'Tweetcord (https://github.com/aetheryx/tweetcord)' }, options.headers),
       path: `/${url.join('/')}`
     }, options);
 
-    let output = '';
+    let output = [];
 
     const req = request(options, (res) => {
       res.on('data', (chunk) => {
-        output += chunk;
+        output.push(chunk);
       });
 
       res.on('end', () => {
+        output = Buffer.concat(output);
         try {
           output = JSON.parse(output);
         } catch (_) { } // eslint-disable-line no-empty

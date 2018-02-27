75105b95dc88c97996bcd7f240df81ddec33c49a
aetheryx <zain1@live.nl>
2018-02-28 00:28:58 +0100

    escape Discord markdown in Twitter entities

--- modules/Utils/utils/parseTwitterEntities.js
+++ modules/Utils/utils/parseTwitterEntities.js
@@ -3,6 +3,8 @@ const entityMap = {
   'screen_name': { char: '@', value: 'screen_name', endpoint: '/' }
 };
 
+const escapeMarkdown = /(_|\*|~)/g
+
 module.exports = function parseTwitterEntities (body, entities) {
   for (const entityType of Object.values(entities)) {
     for (const entity of entityType) {
@@ -11,7 +13,7 @@ module.exports = function parseTwitterEntities (body, entities) {
           const info = entityMap[parser];
           body = body.replace(
             new RegExp(info.char + entity[info.value], 'i'),
-            `[${info.char}${entity[info.value]}](https://twitter.com${info.endpoint}${entity[info.value]})`
+            `[**${info.char}${entity[info.value].replace(escapeMarkdown, '\\$1')}**](https://twitter.com${info.endpoint}${entity[info.value]})`
           );
         }
       }

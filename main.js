e487d427d7a8fe8dd20202da51cab5280fe69837
aetheryx <zain1@live.nl>
2018-02-28 18:04:30 +0100

    fix license (apparently that's just a template...)

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+/**
+ *  Tweetcord: A Discord bot that allows you to interact with Twitter
+ *  Copyright (C) 2018 Aetheryx (zainalih3c@gmail.com)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const botPackage = require(`${__dirname}/package.json`);
 const modules = require(`${__dirname}/modules`);
 

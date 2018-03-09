d89b7e51e922429da2c122ce0a871df47ec2aa98
aetheryx <zain1@live.nl>
2018-03-09 15:15:26 +0100

    add `v` to bot and mongo version, add https

--- modules/Bot/commands/stats.js
+++ modules/Bot/commands/stats.js
@@ -1,6 +1,6 @@
 const os = require('os');
 const totalMem = os.totalmem();
-const { VERSION } = require('eris');
+const { VERSION: erisVersion } = require('eris');
 const { version: mongoVersion } = require('mongodb/package.json');
 
 const inline = true;
@@ -12,7 +12,7 @@ async function statsCommand (msg) {
     : this.bot.shards.get(0);
 
   return {
-    title: `Tweetcord ${this.package.version}`,
+    title: `Tweetcord v${this.package.version}`,
     url: 'https://tweetcord.xyz',
     fields: [
       { name: 'Guilds', value: this.bot.guilds.size, inline },
@@ -20,9 +20,9 @@ async function statsCommand (msg) {
       { name: 'Ping', value: `${shard.latency.toFixed()} ms`, inline },
       { name: 'Libraries',
         value: [
-          `[Eris](https://abal.moe/Eris) v${VERSION}`,
+          `[Eris](https://abal.moe/Eris) v${erisVersion}`,
           `[Node.js](https://nodejs.org/en/) ${process.version}`,
-          `[MongoDB](https://github.com/mongodb/node-mongodb-native/tree/3.0.0) ${mongoVersion}`
+          `[MongoDB](https://github.com/mongodb/node-mongodb-native/tree/3.0.0) v${mongoVersion}`
         ].join('\n'),
         inline },
       { name: 'RAM Usage', value: `${(memUsage / 1048576).toFixed()}MB/${(totalMem / 1073741824).toFixed(1)} GB\n(${(memUsage / totalMem * 100).toFixed(2)}%)`, inline },
@@ -32,7 +32,7 @@ async function statsCommand (msg) {
           `[Bot invite](https://discordapp.com/oauth2/authorize?permissions=27648&scope=bot&client_id=${this.bot.user.id})`,
           '[Support server invite](https://discord.gg/Yphr6WG)',
           '[GitHub](https://github.com/aetheryx/tweetcord)',
-          '[Website](http://tweetcord.xyz)'
+          '[Website](https://tweetcord.xyz)'
         ].join(' | ') }
     ],
     footer: { text: 'Created by Aetheryx#2222' }

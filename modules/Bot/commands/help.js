38e41b284e536835c7db4c7ef6ea06ce35c048c4
aetheryx <zain1@live.nl>
2018-03-09 15:09:11 +0100

    Add prefix to help command for this guy https://why-are-you-buying-clothes-at.the-soup.store/1b2442.png

--- modules/Bot/commands/help.js
+++ modules/Bot/commands/help.js
@@ -9,7 +9,7 @@ async function helpCommand (msg, args) {
   const prefix = await this.db.getPrefix(msg.channel.guild ? msg.channel.guild.id : null);
 
   if (!args[0]) {
-    const content = 'Run the `link` command to link your Twitter account, and then use the `stream` command to set up a stream of events to a specific channel.\nHere\'s a list of all of my commands:';
+    const content = `My prefix in this server is \`${prefix}\`, but you can also mention me.\n\nTo get started, run the \`${prefix}link\` command to link your Twitter account, and then use the \`${prefix}stream\` command to set up a stream of events to a specific channel.\nHere's a list of all of my commands:`;
 
     return {
       content,

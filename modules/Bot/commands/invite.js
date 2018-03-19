6fdcd7a3c56c7cd5277f9208a1b5e70bde390d8c
aetheryx <zain1@live.nl>
2018-03-19 17:24:14 +0100

    oops

--- modules/Bot/commands/invite.js
+++ modules/Bot/commands/invite.js
@@ -1,7 +1,7 @@
 async function inviteCommand (msg, args) {
   const IDArgument = msg.mentions[0] ? msg.mentions[0].id : args[0];
   return {
-    description: `Click [here](https://discordapp.com/oauth2/authorize?permissions=27712&scope=bot&client_id=${msg.author.id === this.config.ownerID ? IDArgument : this.bot.user.id}) to invite me to your server, ` +
+    description: `Click [here](https://discordapp.com/oauth2/authorize?permissions=27712&scope=bot&client_id=${msg.author.id === this.config.bot.ownerID ? IDArgument : this.bot.user.id}) to invite me to your server, ` +
       'or click [here](https://discord.gg/Yphr6WG) for an invite to Tweetcord\'s support server.'
   };
 }

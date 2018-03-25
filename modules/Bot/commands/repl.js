1b600699fb65a4d4877f74dfcde47326cb9c6ee3
aetheryx <zain1@live.nl>
2018-03-25 23:45:50 +0200

    move to our own custom lib (https://npmjs.org/package/programmatic-repl come take a peek!)

--- modules/Bot/commands/repl.js
+++ modules/Bot/commands/repl.js
@@ -1,84 +1,52 @@
 const { inspect } = require('util');
-const { createContext, runInContext } = require('vm');
+const ProgrammaticREPL = require('programmatic-repl'); // Our own module :')
 
 async function replCommand (msg) {
-  let ctx = { ...this }; // Shallow clone
-  createContext(ctx);
+  const REPL = new ProgrammaticREPL({
+    includeNative: true,
+    includeBuiltinLibs: true,
+    stringifyResults: true,
+    name: 'tweetcord.repl'
+  }, {
+    ...this
+  });
 
-  let lastRanCommandOutput;
-  let statementQueue = [];
-  let openingBrackets = 0;
-  let closingBrackets = 0;
   const runCommand = async () => {
-    const commandMsg = await this.bot.MessageCollector.awaitMessage(msg.channel.id, msg.author.id, 60e3);
+    const commandMsg = await this.bot.MessageCollector.awaitMessage(msg.channel.id, msg.author.id, 120e3);
     if (!commandMsg) {
       return this.bot.sendMessage(msg.channel.id, 'Timed out, automatically exiting REPL...');
     }
 
-    let { content } = commandMsg;
-
-    if (content.startsWith('//')) {
-      return runCommand();
-    }
-    if (content === '.exit') {
+    if (commandMsg.content === '.exit') {
       return this.bot.sendMessage(msg.channel.id, 'Successfully exited.');
     }
-    if (content === '.clear') {
-      ctx = { ...this };
-      createContext(ctx);
-      statementQueue = [];
-      this.bot.sendMessage(msg.channel.id, 'Successfully cleared variables.');
-      return runCommand();
-    }
 
-    ctx.msg = commandMsg;
-    ctx._ = lastRanCommandOutput;
-
-    if (content.endsWith('}') && statementQueue[0]) {
-      closingBrackets++;
-      if (closingBrackets === openingBrackets) {
-        // Matching Closing and Opening brackets - we consume the statement queue
-        statementQueue.push(content);
-        content = statementQueue.join('\n');
-        statementQueue = [];
-        closingBrackets = 0;
-        openingBrackets = 0;
-      } else {
-        statementQueue.push(content);
-        this.bot.sendMessage(msg.channel.id, `\`\`\`js\n${statementQueue.join('\n')}\n  ...\n\`\`\``);
-        return runCommand();
-      }
-    } else if (content.endsWith('{') || statementQueue[0]) {
-      if (content.endsWith('{')) openingBrackets++;
-      if (content.endsWith('}') || content.startsWith('}')) closingBrackets++;
-      // Opening bracket - we either open the statement queue or append to it
-      statementQueue.push(content.endsWith('{')
-        ? content
-        : '  ' + content); // Indentation for appended statements
-      this.bot.sendMessage(msg.channel.id, `\`\`\`js\n${statementQueue.join('\n')}\n  ...\n\`\`\``);
-      return runCommand();
-    }
+    REPL.ctx.msg = commandMsg;
 
     let result;
     try {
-      result = await runInContext(content, ctx, {
-        filename: 'aetheryx.repl'
-      });
-
-      lastRanCommandOutput = result;
-
-      if (typeof result !== 'string') {
-        result = inspect(result, {
-          depth: +!(inspect(result, { depth: 1 }).length > 1990), // Results in either 0 or 1
-          showHidden: true
-        });
-      }
+      result = await REPL.execute(commandMsg.content);
     } catch (e) {
       const error = e.stack || e;
       result = `ERROR:\n${typeof error === 'string' ? error : inspect(error, { depth: 1 })}`;
     }
 
-    this.bot.sendMessage(msg.channel.id, '```js\n' + this.misc.redact(result) + '\n```');
+    if (typeof result !== 'string') {
+      result = inspect(result, {
+        depth: +!(inspect(result, { depth: 1, showHidden: true }).length > 1990), // Results in either 0 or 1
+        showHidden: true
+      });
+    }
+
+    result = this.misc.redact(result);
+    if (result.length > 1980) {
+      // If it's over the 2k char limit, we break off the result, pop the last line and close off
+      result = result.slice(0, 1980).split('\n');
+      result.pop();
+      result = result.join('\n') + '\n\n...';
+    }
+
+    this.bot.sendMessage(msg.channel.id, '```js\n' + result + '\n```');
 
     runCommand();
   };
